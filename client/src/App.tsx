77bd6be6dfc776b540a1351f0fa82d7b7214ed1a
dlwl98 <ff981113@hanyang.ac.kr>
2023-03-28 01:31:07 +0900

    chore: vite dev 프록시 설정 테스트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    axios.get("/api/hello").then((res) => console.log(res.data));
+  },[])
 
   return (
     <div className="App">
