b2e96f537916fab3eb6fbe46494b692b43af751b
dlwl98 <ff981113@hanyang.ac.kr>
2023-03-28 00:39:32 +0900

    chore: nestjs globalprefix 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(app.get(ConfigService).get('PORT'));
 }
 bootstrap();
