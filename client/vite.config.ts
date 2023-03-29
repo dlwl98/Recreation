a544fcddffc681be5adaa6cc5b158cd48529b5ce
dlwl98 <ff981113@hanyang.ac.kr>
2023-03-29 17:35:44 +0900

    chore: prettier, eslint 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8080/',
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
