ecc6ee5395024a5b6736b4e2967e5b87669ba6c0
dlwl98 <ff981113@hanyang.ac.kr>
2023-04-03 16:20:44 +0900

    chore: env 파일 생성 및 api-url 숨김처리

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/',
+        target: process.env.API_URL,
       },
     },
   },
