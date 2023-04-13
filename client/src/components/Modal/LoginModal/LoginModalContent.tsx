7e98a59ee9dee57f7817cb21cc9daacbdc770081
dlwl98 <ff981113@hanyang.ac.kr>
2023-04-13 17:47:34 +0900

    rename: 폴더명 수정

--- client/src/components/Modal/LoginModal/LoginModalContent.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const LoginModalContent = styled.div`
-  background-color: white;
-  padding: 30px;
-  border-radius: 5px;
-`;
-
-export default LoginModalContent;
