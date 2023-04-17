7b5c13212f9960813aeb9b800c122f6ead729bf8
dlwl98 <ff981113@hanyang.ac.kr>
2023-04-17 22:21:25 +0900

    feat: categories 배열 작성

--- client/src/custom-types/Categories.ts
+++ client/src/custom-types/Categories.ts
@@ -1,5 +1,7 @@
 export type Categories = 'choseong' | '4word' | 'sokdam';
 
+export const categories: Categories[] = ['choseong', '4word', 'sokdam'];
+
 export const categoriesDisplayString = {
   ['choseong']: '초성게임',
   ['4word']: '사자성어',
