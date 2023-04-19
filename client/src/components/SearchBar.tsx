90195046e49aaa0ffb910a3cbd45454e766118be
dlwl98 <ff981113@hanyang.ac.kr>
2023-04-19 17:38:24 +0900

    feat: input태그 포커스시 색변경 스타일 분리

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/css';
 import { useState } from 'react';
 
+import { inputFocusStyle } from '@styles/inputFocusStyle';
 import { theme } from '@styles/theme';
 
 import usePostsOption from '@hooks/usePostsOption';
@@ -12,13 +13,17 @@ import Flex from '@ds/Flex';
 import Input from '@ds/Input';
 
 const SearchBar = () => {
-  const [inputString, setInputString] = useState('');
-  const { setSearch } = usePostsOption();
+  const { option, setSearch } = usePostsOption();
+  const [inputString, setInputString] = useState(option.search);
 
   return (
     <div>
       <Flex align="center" justify="space-between">
-        <Input value={inputString} onChange={(e) => setInputString(e.target.value)} />
+        <Input
+          value={inputString}
+          onChange={(e) => setInputString(e.target.value)}
+          className={inputFocusStyle}
+        />
         <ContentMargin size="5px" />
         <div className={searchButtonStyle} onClick={() => setSearch(inputString)}>
           <Icon name="search" fill={0} size="2rem" color={theme.color.gray700} />
